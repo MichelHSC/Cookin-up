bd2130b7e710c0fac28e37538a5d465737a4a6c8
Michel Henrique <michelstcosta@gmail.com>
2024-05-02 18:24:12 -0300

    Cookin_Up

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Banner from './components/Banner.vue'
+  import ConteudoPrincipal from './components/ConteudoPrincipal.vue'
+  
+
+  export default{
+    name: 'App',
+    components:{
+      Banner,
+      ConteudoPrincipal
+    }
+  }
+</script>
+            
+<template>
+
+ <Banner/>
+ 
+ <ConteudoPrincipal/>
+
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,46 @@
+<script>
+import Tag from './Tag.vue';
+
+export default{
+    name: 'IngredienteSelecionavel',
+    components:{
+        Tag
+    },
+    props:{
+        ingrediente:{ type: String, required:true}
+    },
+    data(){
+        return{
+            selecionado: false
+        }
+    },
+    methods:{
+        selected(){
+            this.selecionado = !this.selecionado
+
+            if(this.selecionado){
+                this.$emit('adicionarIngrediente', this.ingrediente);
+            }else if(!this.selecionado){
+                this.$emit('removerIngrediente',this.ingrediente)
+            }
+        }
+    },
+    emits: ['adicionarIngrediente','removerIngrediente']
+}
+
+</script>
+
+<template>
+    <button class="ingrediente" @click="selected">
+        <Tag :texto="ingrediente" :ativa="selecionado"/>
+    </button>
+
+
+</template>
+
+<style scoped>
+.ingrediente{
+    cursor: pointer;
+}
+
+</style>
